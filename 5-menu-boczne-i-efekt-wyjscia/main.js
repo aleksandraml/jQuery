cd6bbfaa9e06e5679683a247612ddfbd3d1e5de3
aleksandraml <aleksandramentel1525@gmail.com>
2023-02-15 11:42:27 +0100

    poprawka

--- 5-menu-boczne-i-efekt-wyjscia/main.js
+++ 5-menu-boczne-i-efekt-wyjscia/main.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
-// console.log('działam')
-$('.hamburger').click(function(){
-$('.main-menu').toggleClass('open');
-});
+$(document).ready(function () {
+    // console.log('działam')
+    $('.hamburger').click(function () {
+        $('.main-menu').toggleClass('open');
+    });
 
-$('main-menu-item a').click(function(e){
-e.prevenyDefault();
+    $('.main-menu-item a').click(function(e) {
+        e.preventDefault();
 
-$('body').fadeOut(2000, function(){
-    window.location.href = $(e.target).attr('href');
-})
-});
+        $('body').fadeOut(2000, function () {
+            window.location.href = $(e.target).attr('href');
+        })
+    });
 
 });
